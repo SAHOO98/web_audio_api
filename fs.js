503dabd1e81d3006b36a1e547467e4dc4dc39e6d
SAHOO98 <uzumaki.sahoo@gmail.com>
2018-01-30 02:19:00 +0530

    new edits

--- fs.js
+++ fs.js
@@ -1,12 +1,16 @@
 let actx;
 let source;
 let gainNode;
-let div
+let div1,div2;
+let osc;
 function change(val){
-  div.innerHTML = val;
+  div1.innerHTML = "Track gain:"+val;
   gainNode.gain.value = val;
 }
-
+function osc_control(val) {
+    div2.innerHTML = "Oscillator frequency :"+val;
+    osc.frequency.value = val;
+}
 function init(){
   try{
     actx = new AudioContext();
@@ -14,10 +18,15 @@ function init(){
     console.log('Error '+e);
   }
 
-  div = document.querySelector('.out');
-  source = load_buffer('src3.mp3');
+  div1 = document.querySelector('.out');
+  div2 = document.querySelector('.osc_freq');
+  source = load_buffer('src2.mp3');
   gainNode = actx.createGain();
   gainNode.gain.value = document.querySelector('input[type="range"]').value;
+
+  osc = actx.createOscillator();
+  osc.type = "sine";
+  osc.frequency.value =document.querySelector('#freq_control').value ;
 
 }
 
@@ -56,7 +65,9 @@ function load_buffer(url){
 function play(){
   source.connect(gainNode);
   gainNode.connect(actx.destination);
-  source.start();
+  osc.connect(actx.destination);
+  osc.start();
+  source.start(0);
 }
 
 function pause(){
